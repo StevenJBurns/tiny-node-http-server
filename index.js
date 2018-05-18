3f31976fdc9578594d02a3a57ad5357250b71ea9
StevenJBurns <stevenjburns@msn.com>
2018-05-18 11:46:37 -0600

    More server building; More console.log output; response returns tiny html

--- index.js
+++ index.js
@@ -8,9 +8,14 @@ const chalk = require("chalk");
 const nodeServer = http.createServer(handleRequest);
 
 function handleRequest(req, res) {
+  let { headers, method, url } = req
+
   console.log(chalk.bgBlue.black(`\n Incoming ${req.method} Request -- URL: ${req.url} `));
-  
+
+  console.log(headers);
+    
   res.writeHead(200);
+  res.write(`<html>\n<body>\n<h1>${req.method}</h1>\n</body>\n</html>`)
   res.end();
   console.log(chalk.bgGreen.black(` Outgoing Request -- STATUS: 200 `));
 }
