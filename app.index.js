ddc5151dd9e63bb6e50aea4140ace1102de067bf
StevenJBurns <stevenjburns@msn.com>
2018-05-23 18:56:37 -0600

    Screwing around with HTTPS; no luck -- need certificates or something

--- app.index.js
+++ app.index.js
@@ -108,8 +108,8 @@ function handleRequest(req, res) {
 
 nodeServer.on("close", () => console.clear());
 
-nodeServer.listen(process.env.SERVER_PORT, () => {
+nodeServer.listen(process.env.PORT_HTTP, () => {
   console.clear();
-  console.log(chalk.inverse(` Server running. Listening on port ${process.env.SERVER_PORT} `));
+  console.log(chalk.inverse(` Server running. Listening on port ${process.env.PORT_HTTP} `));
   console.log(chalk.bgBlackBright.white(` Public Folder : ${pathPublic} `));
 })
